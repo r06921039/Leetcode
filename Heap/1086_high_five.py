486fff8b77114c37a1fd6f44909ff3ddd3d9b441
Jeff Lu <ml2684@cornell.edu>
2021-08-11 16:51:14 -0400

    add javascript 1086

--- Heap/1086_high_five.py
+++ Heap/1086_high_five.py
@@ -28,3 +28,34 @@ class Solution:
         ans.sort()
         
         return ans
+    
+    
+    
+"""
+Javascript
+"""
+var highFive = function(items) {
+    var dict = {};
+    for (item of items){
+        var key = item[0];
+        var value = item[1];
+        if (key in dict){
+            dict[key].enqueue(value);
+            if (dict[key].size() > 5){
+                dict[key].dequeue(); 
+            }
+        }
+        else{
+            dict[key] = new MinPriorityQueue();
+            dict[key].enqueue(value);
+        }
+    }
+    let ans = [];
+    for (key in dict){
+        var arr = dict[key].toArray();
+        var sum = arr.reduce((total, value) => total + Number(value["element"]), 0);
+        ans.push([Number(key), Math.floor(sum/5)]);
+    }
+    ans.sort((a,b) => a[0]-b[0]);
+    return ans;
+};
