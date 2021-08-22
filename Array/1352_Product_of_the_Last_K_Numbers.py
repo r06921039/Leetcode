9fb6beccfc93fdeeaaaa40d9318b2cbb92fe03fc
Jeff Lu <ml2684@cornell.edu>
2021-08-21 22:50:41 -0400

    1352

---
+++ Array/1352_Product_of_the_Last_K_Numbers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 22:47:51 2021
+
+@author: jeff
+"""
+
+"""
+Key Idea:
+    Store prefix product
+    if num == 0 then set stack to [1]
+    else append stack[-1] * num
+    len() is constant
+"""
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.stack = [1]
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.stack = [1]
+        else:
+            self.stack.append(self.stack[-1] * num)
+        
+
+    def getProduct(self, k: int) -> int:
+        if k >= len(self.stack):
+            return 0
+        else:
+            return self.stack[-1] // self.stack[-k-1]
